b175be78ef95e7117a088627166ad09b7b13ddbc
Bjbajra <70744118+Bjbajra@users.noreply.github.com>
2021-12-21 11:23:12 +0100

    LittleRefactoring

---
+++ Lab1B-Mocking.Tests/ReverseManipulatorTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1B_Mocking.Tests
+{ 
+    [TestFixture]
+    public class ReverseManipulatorTests
+    {
+        [Test]
+        public void ReverseManipulator_GivenText_ReverseText()
+        {
+            //Arrange
+            ReverseManipulator manipulator = new ReverseManipulator();
+
+            //Act
+            var result = manipulator.Manipulate("mocking");
+
+            //Assert
+            Assert.That(result, Is.EqualTo("gnikcom"));
+
+        }
+    }
+}
---
+++ Lab1B-Mocking.Tests/StringModifierTests.cs
@@ -0,0 +1,52 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1B_Mocking.Tests
+{ 
+    [TestFixture]
+    public class StringModifierTests
+    {
+        private Mock<IStringDataProvider> _provider;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _provider = new Mock<IStringDataProvider>();
+        }
+
+        [Test]
+        public void StringModifier_GivenReverseManipulator_ReturnsReversedText()
+        {
+            //Arrange
+            _provider.Setup(x => x.Read()).Returns("Bijay");
+            IStringManipulator manipulator = new ReverseManipulator();
+            StringModifier modifier = new StringModifier(_provider.Object, manipulator);
+
+            //Act
+            var result = modifier.Run();
+
+            //Assert
+            Assert.That(result, Is.EqualTo("yajiB"));
+        }
+
+        [Test]
+        public void StringModifier_GivenToUpperCaseManipulator_ReturnsUPPERCASETEXT()
+        {
+            //Arrange
+            _provider.Setup(x => x.Read()).Returns("Bijay");
+            IStringManipulator manipulator = new ToUpperCaseManipulator();
+            StringModifier modifier = new StringModifier(_provider.Object, manipulator);
+
+            //Act
+            var result = modifier.Run();
+
+            //Assert
+            Assert.That(result, Is.EqualTo("BIJAY"));
+        }
+    }
+}
